eb8b5f86f4db599b27de8213375b0ea83377ad19
kavita13feb <bsckavita@gmail.com>
2023-09-26 13:52:02 +0530

    searchbar

---
+++ ap-task/src/Pages/VedioSeries.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SearchBar } from '../Components/SearchBar'
+import { Banner } from '../Components/Banner'
+
+export const VedioSeries = () => {
+  return (
+    <div>
+        <SearchBar/>
+        <Banner/>
+
+    </div>
+  )
+}
